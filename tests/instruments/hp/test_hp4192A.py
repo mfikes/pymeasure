1b1b6f6fe2464f0197c98d471ea87f80418252bd
Mike Fikes <mike@fikesfarm.com>
2022-01-05 20:40:15 -0500

    wip

---
+++ tests/instruments/hp/test_hp4192A.py
@@ -0,0 +1,78 @@
+#
+# This file is part of the PyMeasure package.
+#
+# Copyright (c) 2013-2021 PyMeasure Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+import time
+
+import pytest
+import math
+from pymeasure.instruments.hp.hp4192A import HP4192A
+
+pytest.skip('Only work with connected hardware', allow_module_level=True)
+
+
+class TestHP4192A:
+    """
+    Unit tests for HP4192A class.
+    This test suite, needs the following setup to work properly:
+        - A HP4192A device should be connected to the computer;
+        - The device's address must be set in the RESOURCE constant;
+    """
+
+    ##################################################
+    # HP4192A device address goes here:
+    RESOURCE = "USB0::10893::6039::CN57266430::INSTR"
+    ##################################################
+
+    #########################
+    # PARAMETRIZATION CASES #
+    #########################
+
+    CHANNELS = [1, 2]
+    RELAYS = [1, 2, 3, 4]
+    BOOLEANS = [False, True]
+    FREQUENCIES = [5, 120, 1000, 10000, 100000, 1000000, 13000000]
+
+    INSTR = HP4192A(RESOURCE)
+
+    ############
+    # FIXTURES #
+    ############
+
+    @pytest.fixture
+    def instr(self):
+        self.INSTR.reset()
+        return self.INSTR
+
+    #########
+    # TESTS #
+    #########
+
+    @pytest.mark.parametrize("case", FREQUENCIES)
+    def test_spot_frequency(self, instr, case):
+        instr.spot_frequency = case
+        assert instr.spot_frequency == case
+
+    @pytest.mark.parametrize("case", FREQUENCIES)
+    def test_start_frequency(self, instr, case):
+        instr.start_frequency = case
+        assert instr.start_frequency == case
